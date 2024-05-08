55ff3d92bad82072aebc4b070006164077aa3097
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-08 23:45:02 +0300

    update warning temp

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ for day in weather_each_day:
         condition = hour["condition"]["text"]
         temp_c = float(hour["temp_c"])
 
-        if temp_c >= 24.0:
+        if temp_c >= 35.0:
             client = Client(twilio_sid, twilio_auth_token)
             message = client.messages.create(
                 body=f"Too hot in {day["date"]} at {time}, the temperature is {temp_c}",
-                from_="+18289002393",
-                to="+962796143741"
+                from_="Your Twilio Number",
+                to="Your Destination Number"
             )
             print(message.status)
